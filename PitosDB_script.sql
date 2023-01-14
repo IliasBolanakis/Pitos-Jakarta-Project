a620a317c62c14fbac7c0680b7ca566f5d9103b2
Ilias Bolanakis <elias.bolanakes@gmail.com>
2023-01-14 19:33:37 +0200

    DB script

--- PitosDB_script.sql
+++ PitosDB_script.sql
@@ -1,9 +1,12 @@
--- Script for Pitos-Web-Project database
+-- Database Script for Pitos web project
 
 drop database if exists pitosdb;
 create database pitosdb;
 
+
 use pitosdb;
+
+
 CREATE TABLE `pie` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
@@ -21,6 +24,39 @@ CREATE TABLE `area` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+CREATE TABLE `role` (	
+	`role_id` int NOT NULL,
+    `description` varchar(45) NOT NULL,
+    PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `fullName` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `tel` varchar(50) default null,
+  `status` varchar(50),
+  `code` varchar(50) default null,
+  `session` varchar(50) default null,
+  `salt` varchar(50) default "abcd1234",
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `user_role` (
+	`user_id` int not null,
+    `role_id` int not null,
+    KEY `user_id_fk_user` (`user_id`),
+    KEY `role_id_fk_role` (`role_id`),
+	CONSTRAINT `id_fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+	CONSTRAINT `role_id_fk_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)  ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 CREATE TABLE `order` (
   `id` int NOT NULL AUTO_INCREMENT,
   `fullname` varchar(200) NOT NULL,
@@ -32,9 +68,12 @@ CREATE TABLE `order` (
   `offer` tinyint DEFAULT NULL,
   `payment` varchar(20) DEFAULT NULL,
   `stamp` datetime NOT NULL,
+  `user_id` int,
   PRIMARY KEY (`id`),
   KEY `order_fk_area` (`area_id`),
-  CONSTRAINT `order_fk_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`)
+  KEY `user_id_fk_user` (`user_id`),
+  CONSTRAINT `order_fk_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`),
+  CONSTRAINT `user_id_fk_user` FOREIGN KEY(`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -49,27 +88,11 @@ CREATE TABLE `order_item` (
   CONSTRAINT `order_item_fk_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`),
   CONSTRAINT `order_item_fk_pie` FOREIGN KEY (`pie_id`) REFERENCES `pie` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `role` (	
-	`role_id` int NOT NULL AUTO_INCREMENT,
-    `description` varchar(45) NOT NULL auto_increment,
-    PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `fullname` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `tel` varchar(50) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+ 
+-- INSERTING PIES
+ 
 insert into pie(id, name, price, filename, ingredients)
-values (1, "Σπανακοπιτα", 4, "/images/spanakopita.jpg", "Σπανάκι, Φέτα" );
+values (1, "Σπανακόπιτα", 4, "/images/spanakopita.jpg", "Σπανάκι, Φέτα" );
 
 insert into pie(id, name, price, filename, ingredients)
 values (2, "Μανιταρόπιτα", 5.5, "/images/manitaropita.jpg", "Μανιτάρια, Βούτυρο");
@@ -79,6 +102,8 @@ values (3, "Πρασόπιτα", 3.5, "/images/prasopita.jpg", "Πράσο, Φέ
 
 insert into pie(id, name, price, filename, ingredients)
 values (4, "Μπουρέκι", 4, "/images/boureki.jpg", "Κολοκύθι, Πατάτα" );
+
+-- INSERTING AREAS
 
 insert into area(id, description)
 values (1, "Αμπελόκηποι");
@@ -91,3 +116,27 @@ values (3, "Κέντρο Αθήνας");
 
 insert into area(id, description)
 values (4, "Ζωγράφου");
+
+-- INSERTING ROLES
+
+insert into role(role_id, description)
+values(1, "admin");
+
+insert into role(role_id, description)
+values(2, "customer");
+
+-- INSERTING DUMMY USERS / ADMINS
+
+insert into user(id, username, password, address, fullname, email, status, salt) -- password = "123456"
+values(1, "bob", "423389c5f680c0a71c246fa64ba52d47fc1a7a9531d5f198fd408ba42afba55d", "Nowhere","Bob Dole" , "bob@dole.com", "verified", "hUifhkJ4jkyOye74KPusaA==");
+
+insert into user(id, username, password, address, fullname, email, status, salt) -- password = "123456"
+values(2, "tom", "ff20754904bc12cdc82383593793dd164e4adc4c39294bf9c0fc1daabf47cf57", "Nowhere","Tom Hanks", "tom@hanks.com", "verified", "FmL3wqNEo7+FxMSiN6fDkQ==");
+
+-- INSERTING USER ROLES
+
+insert into user_role(user_id, role_id)
+values(1, 1);
+
+insert into user_role(user_id, role_id)
+values(2, 2);
